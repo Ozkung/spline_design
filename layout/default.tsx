0916030541e023d134553a561a25d4defd1f8536
DeadlyLemonate <pongpop.suw@gmail.com>
2023-04-22 05:12:01 +0700

    ทดสอบ fn from next

--- layout/default.tsx
+++ layout/default.tsx
@@ -5,11 +5,10 @@ type LayoutProps = { children?: ReactNode; type?: String };
 export default function Layout({ children, type }: LayoutProps) {
   return (
     <>
-      add head
       <main>
         <div>
-          navbar
-          {children} ::: {type}
+          navbar ::: {type}
+          {children}
           footer
         </div>
       </main>
--- pages/tammer.tsx
+++ pages/tammer.tsx
@@ -1,12 +1,30 @@
 import styles from "@/styles/Home.module.css";
 import Layout from "@/layout/default";
+import { useState } from "react";
+import Card from "@/components/card";
 
 export default function tammer() {
+  // watch text
+  const [text, setText] = useState("");
   return (
     <>
       <main className={styles.main}>
         <Layout type="atsss">
           <div>Tammer Awaitting</div>
+          <div>
+            Text :&nbsp;
+            <input
+              type="text"
+              value={text}
+              onChange={(e) => {
+                setText(e.currentTarget.value);
+                console.log("currentTarget :", e.currentTarget.value);
+              }}
+            />
+          </div>
+          <div>
+            <Card title="checkmate" />
+          </div>
         </Layout>
       </main>
     </>
